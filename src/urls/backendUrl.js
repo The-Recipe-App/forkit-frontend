bc648fd051fe13532713f9ad514c7483cfdb2cd1
alvin3930 <hemanshuvaidya06@gmail.com>
2026-01-05 10:35:52 +0530

    Update backendUrl.js

--- src/urls/backendUrl.js
+++ src/urls/backendUrl.js
@@ -1,2 +1,3 @@
-const backendUrlV1 = import.meta.env.VITE_BACKEND_URL || "http://127.0.0.1:8000/api/v1/";
+//const backendUrlV1 = import.meta.env.VITE_BACKEND_URL || "http://127.0.0.1:8000/api/v1/";
+const backendUrlV1 = "https://humorous-integrity-production.up.railway.app/api/v1/";
 export default backendUrlV1;

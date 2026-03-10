9fb56220d7aa78cd8a7a30c6daa718c92319d42c
Hemanshu <hemanshuvaidya64@gmail.com>
2026-03-10 12:14:35 +0530

    Delete functions directory

--- functions/admin/[[path]].js
+++
@@ -1,7 +0,0 @@
-import { proxyTo } from '../_shared/proxy.js';
-
-export async function onRequest({ request, params, env }) {
-  const url = new URL(request.url);
-  const path = params.path?.join('/') ?? '';
-  return proxyTo(request, `${env.BACKEND_URL}/admin/${path}${url.search}`);
-}
